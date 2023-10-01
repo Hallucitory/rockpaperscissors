77cfe487feb4846643c0a1756b193ac728e7a8db
Justin <120054366+Hallucitory@users.noreply.github.com>
2023-10-01 12:48:39 -0500

    make one round game with opponent input

--- script.js
+++ script.js
@@ -9,4 +9,23 @@ function getComputerchoice() {
         return 'Scissors';
     }
 }
-console.log(getComputerchoice());
+
+
+const playerSelection = "Rock";
+const computerSelection = getComputerchoice();
+
+const playerWin = ("You win! " + playerSelection + " beats " + computerSelection.toLowerCase() + ".");
+const playerLose = ("You lose! " + computerSelection + " beats " + playerSelection.toLowerCase() + ".");
+const playerTie = ("You tie! " + playerSelection + " ties with " + computerSelection.toLowerCase() + ".");
+
+function playRound(playerSelection, computerSelection){
+    if ((playerSelection.toLowerCase() === "rock") && (computerSelection.toLowerCase() === "paper")){
+       return playerWin;
+    } else if ((playerSelection.toLowerCase() === "rock") && (computerSelection.toLowerCase() === "rock")){
+        return playerTie;
+    } else {
+        return playerLose;
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
